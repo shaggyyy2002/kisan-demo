03c12fe0a2be465a996dc2855634ddcf3d549877
shaggyyy2002 <nitinagouda@acpce.ac.in>
2023-10-31 12:33:07 +0530

    final project

--- src/main/java/com/example/jaikisandemo/hello.java
+++ src/main/java/com/example/jaikisandemo/hello.java
@@ -3,10 +3,11 @@ package com.example.jaikisandemo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hello")
 @RestController
-    public class hello {
-        public String helloJaiKisan() {
-            return "Hello Jai Kisan Team";
-        }
+public class hello {
+
+    @RequestMapping("/hello")    
+    public String helloJaiKisan() {
+        return "<h1>Hello Jai Kisan Team, </h1><br>  <h2>This is Nitin's DevOps take away assignment demo</h2>";
+    }
 }
